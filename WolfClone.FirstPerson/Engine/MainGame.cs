c5f97a255c4f7a024ede0855a37df729b8b9c46c
Nonparoxysmic <69494951+Nonparoxysmic@users.noreply.github.com>
2025-04-09 12:16:50 -0500

    Create new project updating to MonoGame 3.8.3

---
+++ WolfClone.FirstPerson/Engine/MainGame.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace WolfClone.FirstPerson.Engine;
+
+public class MainGame : Game
+{
+    private readonly GraphicsDeviceManager _graphics;
+    private SpriteBatch _spriteBatch;
+
+    public MainGame()
+    {
+        _graphics = new GraphicsDeviceManager(this);
+        Content.RootDirectory = "Content";
+        IsMouseVisible = true;
+        Window.AllowUserResizing = true;
+        Window.Title = "WolfClone development";
+    }
+
+    protected override void Initialize()
+    {
+        base.Initialize();
+    }
+
+    protected override void LoadContent()
+    {
+        _spriteBatch = new SpriteBatch(GraphicsDevice);
+    }
+
+    protected override void Update(GameTime gameTime)
+    {
+        double deltaTime = gameTime.ElapsedGameTime.TotalSeconds;
+
+        Vector2 directionalInput = Vector2.Zero;
+        KeyboardState keyboardState = Keyboard.GetState();
+        if (keyboardState.IsKeyDown(Keys.Up) || keyboardState.IsKeyDown(Keys.W))
+        {
+            directionalInput.Y -= 1;
+        }
+        if (keyboardState.IsKeyDown(Keys.Down) || keyboardState.IsKeyDown(Keys.S))
+        {
+            directionalInput.Y += 1;
+        }
+        if (keyboardState.IsKeyDown(Keys.Left) || keyboardState.IsKeyDown(Keys.A))
+        {
+            directionalInput.X -= 1;
+        }
+        if (keyboardState.IsKeyDown(Keys.Right) || keyboardState.IsKeyDown(Keys.D))
+        {
+            directionalInput.X += 1;
+        }
+
+        // TODO: Add update logic here
+
+        base.Update(gameTime);
+    }
+
+    protected override void Draw(GameTime gameTime)
+    {
+        GraphicsDevice.Clear(Color.CornflowerBlue);
+
+        // TODO: Add drawing code here
+
+        base.Draw(gameTime);
+    }
+}
---
+++ WolfClone.FirstPerson/Program.cs
@@ -0,0 +1,2 @@
+﻿using var game = new WolfClone.FirstPerson.Engine.MainGame();
+game.Run();
